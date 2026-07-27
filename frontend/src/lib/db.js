6723d2664af0c12702ce07504374b0cc8b715b93
VictorHugo-Neo <prfvhlc@gmail.com>
2026-07-27 19:58:40 -0300

    fix: corrigir lib para conexão local e vps

--- frontend/src/lib/db.js
+++ frontend/src/lib/db.js
@@ -2,14 +2,16 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
+const connectionString = import.meta.env.DATABASE_URL || process.env.DATABASE_URL;
+
+console.log('DATABASE_URL carregada?', !!connectionString);
 
 export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: connectionString,
 });
 
 pool.on('connect', () => {
-    console.log('Conectado ao PostgreSQL!');
+    console.log('Conectado ao PostgreSQL com sucesso!');
 });
 
 pool.on('error', (err) => {
